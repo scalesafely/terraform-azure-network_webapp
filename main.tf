7b49ae0ecd6680776c5d33aba0695413ae9bdb71
Soniaayem <sonia.ayem@scafe.io>
2023-09-21 14:16:59 +0200

    1.0.0

--- main.tf
+++ main.tf
@@ -61,13 +61,13 @@ resource "azurerm_private_dns_zone" "pv_st1_dns" {
 }
 
 // Private Endpoint storage
-resource "azurerm_private_endpoint" "storage-endpoint" {
+resource "azurerm_private_endpoint" "storage-endpoint1" {
   name                = "stc1-endpoint"
   location            = var.location
   resource_group_name = var.resource_group_name
   subnet_id           = azurerm_subnet.data_subnet.id
   private_service_connection {
-    name                           = "storage-pc2"
+    name                           = "storage-pc1"
     private_connection_resource_id = var.storage_account_id
     subresource_names              = ["file"]
     is_manual_connection           = false
@@ -92,7 +92,7 @@ resource "azurerm_private_endpoint" "storage-endpoint2" {
   subnet_id           = azurerm_subnet.data_subnet.id
   private_service_connection {
     name                           = "storage-pc2"
-    private_connection_resource_id = var.storage_account_id
+    private_connection_resource_id = var.stac_webapp_id
     subresource_names              = ["file"]
     is_manual_connection           = false
   }
@@ -111,24 +111,24 @@ resource "azurerm_private_endpoint" "storage-endpoint2" {
 resource "azurerm_private_dns_zone_virtual_network_link" "dns-zone-to-vnet-link" {
   name                  = "vnet-link"
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.pv_dns.name
+  private_dns_zone_name = azurerm_private_dns_zone.pv_st_dns.name
   virtual_network_id    = azurerm_virtual_network.Vnet.id
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "st1-dns-zone-to-vnet-link" {
   name                  = "st1-vnet-link"
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.pv_st_dns.name
+  private_dns_zone_name = azurerm_private_dns_zone.pv_st1_dns.name
   virtual_network_id    = azurerm_virtual_network.Vnet.id
 }
 
 
 resource "azurerm_private_dns_a_record" "st1filerecord" {
   name                = var.storage_account1_name
-  zone_name           = azurerm_private_dns_zone.pv_st_dns.name
+  zone_name           = azurerm_private_dns_zone.pv_st1_dns.name
   resource_group_name = var.resource_group_name
   ttl                 = 300
-  records             = [azurerm_private_endpoint.storage-endpoint.private_service_connection[0].private_ip_address]
+  records             = [azurerm_private_endpoint.storage-endpoint1.private_service_connection[0].private_ip_address]
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "st2-dns-zone-to-vnet-link" {
@@ -140,9 +140,9 @@ resource "azurerm_private_dns_zone_virtual_network_link" "st2-dns-zone-to-vnet-l
 
 
 resource "azurerm_private_dns_a_record" "st2filerecord" {
-  name                = var.storage_account2_name
+  name                = "stac_webapp"
   zone_name           = azurerm_private_dns_zone.pv_st2_dns.name
   resource_group_name = var.resource_group_name
   ttl                 = 300
-  records             = [azurerm_private_endpoint.storage-endpoint.private_service_connection[0].private_ip_address]
+  records             = [azurerm_private_endpoint.storage-endpoint2.private_service_connection[0].private_ip_address]
 }
